0f8057cc778fb267a69f6dd7a979ec33ea7f44de
Ben Hunter <learntocode@benhunter.me>
2021-09-30 09:58:38 -0500

    Spring Initializr

---
+++ src/main/java/com/benhunterlearn/springcrudtodo/SpringCrudTodoApplication.java
@@ -0,0 +1,13 @@
+package com.benhunterlearn.springcrudtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benhunterlearn/springcrudtodo/SpringCrudTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benhunterlearn.springcrudtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
